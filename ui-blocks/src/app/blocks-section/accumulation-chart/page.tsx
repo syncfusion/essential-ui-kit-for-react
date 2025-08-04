a121b7576f52d1ffb99b82d6cf77eb8697fe6655
E.Hari Venkatesh <harivenkateshe@syncfusion.com>
2025-08-04 16:25:37 +0530

    Update blocks and fix bugs for v7.0.0 release.

--- ui-blocks/src/app/blocks-section/accumulation-chart/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import Blocks from "../../main-section/blocks/blocks";
-
-export default function AccumulationChart() {
-  return (
-    <Blocks blockName="accumulation-chart" />
-  );
-}
